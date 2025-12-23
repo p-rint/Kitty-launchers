0902815a7adafef8d156a977def4028b69ddd6df
p-rint <159627419+p-rint@users.noreply.github.com>
2025-12-23 00:52:08 -0500

    Gave ze cat animations
    anim tree stuff

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -15,18 +15,22 @@ const JUMP_VELOCITY = 4.5
 
 var dt : float
 
+@onready var animTree: AnimationTree = $AnimationTree
+
+
 func flatten(vector: Vector3) -> Vector3:
 	return Vector3( vector.x, 0, vector.z)
 
 func move() -> void:
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = lerp(velocity.x, direction.x * SPEED, .15 * 2)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, .15 * 2)
 		model.rotation.y = lerp_angle(model.rotation.y, atan2(-velocity.x, -velocity.z), .2)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, .15 * 3)
+		velocity.z = move_toward(velocity.z, 0, .15 * 3)
+	
+	animTree.set("parameters/Run/blend_position", velocity.length()/SPEED)
 
 func _physics_process(delta: float) -> void:
 	dt = delta
@@ -43,5 +47,6 @@ func _physics_process(delta: float) -> void:
 	input_dir = Input.get_vector("Left", "Right", "Up", "Down")
 	direction = flatten($CamPivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	move()
-
+	
+	
 	move_and_slide()
